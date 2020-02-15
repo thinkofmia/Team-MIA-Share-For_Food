7c83ee4c57e67a64bdde35a21a8c66dcc8f6875c
FremontTeng <fremontteng@gmail.com>
2020-02-15 15:50:16 +0800

    Added about us page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 showHomePage = function(){
     hideAllPages();
     document.getElementById("homePage").style.display = "inline-block";
-    document.getElementById("maintext").innerHTML = "Starting";
+    document.getElementById("maintext").innerHTML = "Fake Text";
 
 }
 
@@ -10,11 +10,30 @@ showHomePage = function(){
 showAboutUsPage = function(){
     hideAllPages();
     document.getElementById("aboutUsPage").style.display = "inline-block";
-    document.getElementById("maintext").innerHTML = "About Us";
+    document.getElementById("aboutUsText").innerHTML = 
+    "<h1>Team MIA</h1>"+
+    "Members of Team MIA: Fremont, Anson, Nicholas<br><br>"+
+    "<b> Formal problem statement </b><br>"+
+    "<b> Definition of the target</b><br>"+
+    "Climate change is a change in the common weather found in a location, which can be measured as a difference in how much rain a place usually gets in a year or a change in a location's normal temperature for a month or season. "+
+    "Leftover food is defined as consumables that remain unused or unconsumed.<br><br>"+
+    "<b>Brief Description</b><br><br>"+
+    "In 2018, the total food wastage generated is up to 763,100 tonnes in Singapore. Food waste accounts for about 10 per cent of the total waste generated in Singapore and those that are disposed of go the waste-to-energy plants for incineration.<br><br>"+
+    "Our submission aims to reduce the amount of food wastage in NTU canteens/restaurants by collecting leftover food and distributing them accordingly to the relevant needy people organizations.<br><br>"+
+    "<b>Outline of the approach</b><br>"+
+    "Collection of the left out food from canteen/restaurant stalls<br>"+
+    "- Gather a list of stalls involved in the project<br>"+
+    "- Setup daily routine of collecting leftover food by their opening hours<br>"+
+    "- Check-in system for leftover food<br><br>"+
+    
+    "<b>Sharing to the needy people organizations</b><br>"+ 
+    "- Alias with the organizations for the food collection<br>"+
+    "- Include expiry dates for the collected food<br>";
 }
 
 //Hide all Pages
 hideAllPages = function(){
     document.getElementById("homePage").style.display = "none";
     document.getElementById("aboutUsPage").style.display = "none";
+    document.getElementById("organizationListPage").style.display = "none";
 }
