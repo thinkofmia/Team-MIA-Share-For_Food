4fd986344515d7eea17a5e9f0b97998e67392c32
thatnicneo <nicholasneo78@gmail.com>
2020-02-16 01:51:05 +0800

    Add another food stall in the "Northspine" section

--- js/organization.js
+++ js/organization.js
@@ -7,20 +7,20 @@ var canteen = "";
 var store = "";
 
 //Organization Page
-becomeOrganizerPage = function(){
+becomeOrganizerPage = function () {
     //Page Effects
     hideAllPages();
     document.getElementById("buttonOrganization").style.backgroundColor = "rgb(0, 255, 0)";
     //Display available locations eg.NTU
-    document.getElementById("maintext").innerHTML = "Choose Location: <br>"+
-    "<div class='selectLocation' id='locationNTU' onclick='displayLocation(\"NTU\")'><u>NTU</u></div>"+
-    "Will be updated soon...<br><br>";
+    document.getElementById("maintext").innerHTML = "Choose Location: <br>" +
+        "<div class='selectLocation' id='locationNTU' onclick='displayLocation(\"NTU\")'><u>NTU</u></div>" +
+        "Will be updated soon...<br><br>";
 }
 
 //Display the location of the selected Campus
-displayLocation = function(location){
+displayLocation = function (location) {
     document.getElementById("subtext1").innerHTML = "";
-    switch (location){
+    switch (location) {
         //NTU Selected
         case "NTU":
             //North Spine
@@ -28,44 +28,45 @@ displayLocation = function(location){
             //South Spine
             displayCanteens("South Spine", "South Spine Food Court<br>Stalls: 2<br>Seating capacity: 830<br>");
 
-           break;
+            break;
         default:
             document.getElementById("subtext1").innerHTML = "There is no food court here!!";
             break;
-    
+
     }
 }
 
 //Display Canteens in boxes
-displayCanteens = function(name, txt){
+displayCanteens = function (name, txt) {
     //Note that name refers store name and txt is the store's flavor text
-    document.getElementById("subtext1").innerHTML += "<div class='store'><div class='storeImg'>"+
-    "<img src='img/"+name+".jpg' alt='"+name+"' onclick=\"displayStores('"+name+"')\"></div>"+txt+"</div>";
+    document.getElementById("subtext1").innerHTML += "<div class='store'><div class='storeImg'>" +
+        "<img src='img/" + name + ".jpg' alt='" + name + "' onclick=\"displayStores('" + name + "')\"></div>" + txt + "</div>";
 }
 
 //Display Stores in boxes
-displayStores = function(canteenName){
+displayStores = function (canteenName) {
     //Save Selected Canteen Name
     canteen = canteenName;
     //Check boxes for Halal, Veg and Allergies
-    document.getElementById("subtext1").innerHTML = 
-    "<input type='checkbox' id='halalCheck' name='Halal'>"+
-    "<label for='halalCheck'> Halal</label><br>"+
-    "<input type='checkbox' id='vegCheck' name='Vegetarian'>"+
-    "<label for='vegCheck'> Vegetarian</label><br>"+
-    "<input type='checkbox' id='allergyCheck' name='Allergies'>"+
-    "<label for='allergyCheck'> Allergies</label><br><br>"+
-    "<input type='submit' value='Search'>";
+    document.getElementById("subtext1").innerHTML =
+        "<input type='checkbox' id='halalCheck' name='Halal'>" +
+        "<label for='halalCheck'> Halal</label><br>" +
+        "<input type='checkbox' id='vegCheck' name='Vegetarian'>" +
+        "<label for='vegCheck'> Vegetarian</label><br>" +
+        "<input type='checkbox' id='allergyCheck' name='Allergies'>" +
+        "<label for='allergyCheck'> Allergies</label><br><br>" +
+        "<input type='submit' value='Search'>";
 
     var noOfStores = 0; //Number of stores in a canteen
-    
+
     //Check which Canteen it is
-    switch (canteen){
+    switch (canteen) {
         //Display Stores in North Spine
         case "North Spine":
-            noOfStores = 2;
+            noOfStores = 3;
             displayIndividualStore("Nicholas Club");
             displayIndividualStore("Anson & Friends");
+            displayIndividualStore("Morgan J Freemont");
             break;
         //By Default display South Spine
         default:
@@ -77,40 +78,45 @@ displayStores = function(canteenName){
 }
 
 //Display each store in boxes
-displayIndividualStore = function(stallName){
+displayIndividualStore = function (stallName) {
     //Default txt
-    var txt = "<div id='storeDescription"+stallName+"'>No Description</div>"+
-                "Food Type: <div id='foodType"+stallName+"'>None</div>"+
-                "Availability: <div id='foodAvailability"+stallName+"'>V</div>";
-    document.getElementById("subtext1").innerHTML += "<div class='store'><div class='storeImg'>"+
-    "<img src='img/"+stallName+".jpg' alt='Stall Img' width='290' height='200' onclick=\"displayFood('"+stallName+"')\"></div>"+txt+"</div>";
+    var txt = "<div id='storeDescription" + stallName + "'>No Description</div>" +
+        "Food Type: <div id='foodType" + stallName + "'>None</div>" +
+        "Availability: <div id='foodAvailability" + stallName + "'>Yes</div>";
+    document.getElementById("subtext1").innerHTML += "<div class='store'><div class='storeImg'>" +
+        "<img src='img/" + stallName + ".jpg' alt='Stall Img' width='290' height='200' onclick=\"displayFood('" + stallName + "')\"></div>" + txt + "</div>";
     //Check stallName and print accordingly
-        switch(stallName){
-            case "Anson & Friends":
-                document.getElementById("storeDescription"+stallName).innerHTML = "AAA";
-                document.getElementById("foodType"+stallName).innerHTML = "Halal";
-                document.getElementById("foodAvailability"+stallName).innerHTML = "Yes";
-                break;
-            case "Nicholas Club":
-                document.getElementById("storeDescription"+stallName).innerHTML = "Nicholas Club";
-                document.getElementById("foodType"+stallName).innerHTML = "Non-Halal";
-                document.getElementById("foodAvailability"+stallName).innerHTML = "Yes";
-                break;
-            case "Wonder Man":
-                    document.getElementById("storeDescription"+stallName).innerHTML = "WW";
-                    document.getElementById("foodType"+stallName).innerHTML = "Vegetarian";
-                    document.getElementById("foodAvailability"+stallName).innerHTML = "Yes";
-                    break;
-            case "Captain Singapore":
-                    document.getElementById("storeDescription"+stallName).innerHTML = "Majulah Ahoy";
-                    document.getElementById("foodType"+stallName).innerHTML = "Non-Halal";
-                    document.getElementById("foodAvailability"+stallName).innerHTML = "Yes";
-                    break;
-        }
+    switch (stallName) {
+        case "Anson & Friends":
+            document.getElementById("storeDescription" + stallName).innerHTML = "AAA";
+            document.getElementById("foodType" + stallName).innerHTML = "Halal";
+            document.getElementById("foodAvailability" + stallName).innerHTML = "Yes";
+            break;
+        case "Nicholas Club":
+            document.getElementById("storeDescription" + stallName).innerHTML = "Nicholas Club";
+            document.getElementById("foodType" + stallName).innerHTML = "Non-Halal";
+            document.getElementById("foodAvailability" + stallName).innerHTML = "Yes";
+            break;
+        case "Wonder Man":
+            document.getElementById("storeDescription" + stallName).innerHTML = "WW";
+            document.getElementById("foodType" + stallName).innerHTML = "Vegetarian";
+            document.getElementById("foodAvailability" + stallName).innerHTML = "Yes";
+            break;
+        case "Captain Singapore":
+            document.getElementById("storeDescription" + stallName).innerHTML = "Majulah Ahoy";
+            document.getElementById("foodType" + stallName).innerHTML = "Non-Halal";
+            document.getElementById("foodAvailability" + stallName).innerHTML = "Yes";
+            break;
+        case "Morgan J Fremont":
+            document.getElementById("storeDescription" + stallName).innerHTML = "Morgan J Freemont";
+            document.getElementById("foodType" + stallName).innerHTML = "Non-Halal";
+            document.getElementById("foodAvailability" + stallName).innerHTML = "Yes";
+            break;
+    }
 }
 
 //Display Food available in each store
-displayFood = function(stallName){
+displayFood = function (stallName) {
     //save the visited store
     store = stallName;
     //Reset global variables
@@ -122,36 +128,36 @@ displayFood = function(stallName){
     //Display Left Over Food Available in check boxes
     document.getElementById("subtext1").innerHTML = "Expected Food Left: <br>";
     document.getElementById("subtext1").innerHTML +=
-    //Display Rice
-    "<input type='checkbox' id='riceCheck' name='Rice'>"+
-    "<label for='riceCheck'> Rice <br>Estimated Amount: 5kg</label><br><br>"+
-    //Display Vegetables
-    "<input type='checkbox' id='vegCheck' name='Vegetables'>"+
-    "<label for='vegeCheck'> Vegetables <br>Estimated Amount: 10kg</label><br><br>"+
-    //Display Meat
-    "<input type='checkbox' id='meatCheck' name='Meat'>"+
-    "<label for='meatCheck'> Meat <br>Estimated Amount: 7kg</label><br><br>"+
-    //Display Soup
-    "<input type='checkbox' id='soupCheck' name='Soup'>"+
-    "<label for='soupCheck'> Soup <br>Estimated Amount: 2kg</label><br><br>"+
-    //The ones that are checked will be available for input later
-    "<input type='submit' value='Search' onclick='organizationVerificationPage()'>";
+        //Display Rice
+        "<input type='checkbox' id='riceCheck' name='Rice'>" +
+        "<label for='riceCheck'> Rice <br>Estimated Amount: 5kg</label><br><br>" +
+        //Display Vegetables
+        "<input type='checkbox' id='vegCheck' name='Vegetables'>" +
+        "<label for='vegeCheck'> Vegetables <br>Estimated Amount: 10kg</label><br><br>" +
+        //Display Meat
+        "<input type='checkbox' id='meatCheck' name='Meat'>" +
+        "<label for='meatCheck'> Meat <br>Estimated Amount: 7kg</label><br><br>" +
+        //Display Soup
+        "<input type='checkbox' id='soupCheck' name='Soup'>" +
+        "<label for='soupCheck'> Soup <br>Estimated Amount: 2kg</label><br><br>" +
+        //The ones that are checked will be available for input later
+        "<input type='submit' value='Search' onclick='organizationVerificationPage()'>";
 
 }
 
-organizationVerificationPage = function(){
+organizationVerificationPage = function () {
     //Initialization
     //Check which variables are checked
     rice = document.getElementById("riceCheck").checked;
     veggie = document.getElementById("vegCheck").checked;
     meat = document.getElementById("meatCheck").checked;
     soup = document.getElementById("soupCheck").checked;
-    
+
     //Display Checkout
     document.getElementById("subtext1").innerHTML = "<form action='/action_page.php'>";
     //Display organization input box and verification Passcode
-    document.getElementById("subtext1").innerHTML +="<b>Organization:</b> <input type='text' id='organizationName' value='Anson and Friends'></input><br>"+
-    "<b>Verification Passcode:</b> <input type='text' id='verifyCode' value='Black Lightning is back'></input><br>";
+    document.getElementById("subtext1").innerHTML += "<b>Organization:</b> <input type='text' id='organizationName' value='Anson and Friends'></input><br>" +
+        "<b>Verification Passcode:</b> <input type='text' id='verifyCode' value='Black Lightning is back'></input><br>";
     //Display food selected
     if (rice)
         document.getElementById("subtext1").innerHTML += "<b>Rice:</b> <input type='text' id='rice'></input>kg<br>";
@@ -162,35 +168,35 @@ organizationVerificationPage = function(){
     if (soup)
         document.getElementById("subtext1").innerHTML += "<b>Soup:</b> <input type='text' id='soup'></input>kg<br>";
     //Display Time to collect and remarks
-    document.getElementById("subtext1").innerHTML +="<b>Collection Time:</b> <input type='text' id='collectionTime' value='Input time to collect'></input><br>"+
-    "<b>Remarks:</b> <input type='text' id='remarks' value='Input remarks'></input><br>";
+    document.getElementById("subtext1").innerHTML += "<b>Collection Time:</b> <input type='text' id='collectionTime' value='Input time to collect'></input><br>" +
+        "<b>Remarks:</b> <input type='text' id='remarks' value='Input remarks'></input><br>";
     document.getElementById("subtext1").innerHTML += "<input type='submit' value='Next' onclick='viewConfirmPage()' '></form>";
 }
 
-viewConfirmPage = function(){
+viewConfirmPage = function () {
     //Remember Organization Name
     var organizationName = document.getElementById("organizationName").value;
     //Create list of food submitted
     var foodOrdered = "";
     if (rice)
-        foodOrdered += "<b>Rice:</b> "+document.getElementById("rice").value+"kg<br>";
+        foodOrdered += "<b>Rice:</b> " + document.getElementById("rice").value + "kg<br>";
     if (veggie)
-        foodOrdered += "<b>Vegetables:</b> "+document.getElementById("veg").value+"kg<br>";
+        foodOrdered += "<b>Vegetables:</b> " + document.getElementById("veg").value + "kg<br>";
     if (meat)
-        foodOrdered += "<b>Meat:</b> "+document.getElementById("meat").value+"kg<br>";
+        foodOrdered += "<b>Meat:</b> " + document.getElementById("meat").value + "kg<br>";
     if (soup)
-         foodOrdered += "<b>Soup:</b> "+document.getElementById("soup").value+"kg<br>";
-    
+        foodOrdered += "<b>Soup:</b> " + document.getElementById("soup").value + "kg<br>";
+
     //Print Confirmations Page
     document.getElementById("subtext1").innerHTML = "";
-    document.getElementById("maintext").innerHTML = "<div class = 'centralizeText'>"+
-    "<h1><u><b>Confirmation Page</u></b></h1>"+
-    "Successfully ordered!<br>"+
-    "ref# "+Math.random()*100000+"<br>"+
-    "Organization: "+organizationName+"<br>"+
-    "Food Ordered: <br>"+foodOrdered+
-    "At "+canteen+", "+store+"<br>"+
-    "<h1>THANK YOU!</h1>";
-    
+    document.getElementById("maintext").innerHTML = "<div class = 'centralizeText'>" +
+        "<h1><u><b>Confirmation Page</u></b></h1>" +
+        "Successfully ordered!<br>" +
+        "ref# " + Math.random() * 100000 + "<br>" +
+        "Organization: " + organizationName + "<br>" +
+        "Food Ordered: <br>" + foodOrdered +
+        "At " + canteen + ", " + store + "<br>" +
+        "<h1>THANK YOU!</h1>";
+
 
 }
