9786340d8d9fbaca13f6825d13b0745bea0dce18
FremontTeng <fremontteng@gmail.com>
2020-02-15 20:45:20 +0800

    Added checks for nothing submitted in foodstore.js

--- js/foodstore.js
+++ js/foodstore.js
@@ -16,12 +16,17 @@ pickFoodStorePage = function(){
     //Verification Code
     document.getElementById("maintext").innerHTML +="<b>Verification Passcode:</b> <input type='text' id='verifyCode'></input><br>";
     //Submit Button
-    document.getElementById("maintext").innerHTML += "<input type='submit' onclick='setFood()' '></form>";
+    document.getElementById("maintext").innerHTML += "<input type='submit' onclick='selectStore()' '></form>";
+}
+
+selectStore = function(){
+    //Set Food Store
+    foodStore.selected = document.getElementById("selectedStore").value;
+    setFood();
 }
 
 //Allows user to select leftover food amount
 setFood = function(){
-    foodStore.selected = document.getElementById("selectedStore").value;
     //Display Food Store Name
     document.getElementById("maintext").innerHTML = foodStore.selected+"<br>"+
     //Display Food Options
@@ -36,14 +41,23 @@ setFood = function(){
 //Confirmation of food store submission
 foodStoreEnd = function(){
     var foodDelievered = "";
-    if (document.getElementById("rice").value>0)
-        foodDelievered += "<b>Rice:</b> "+document.getElementById("rice").value+"kg<br>";
-    if (document.getElementById("veg").value>0)
-        foodDelievered += "<b>Vegetables:</b> "+document.getElementById("veg").value+"kg<br>";
-    if (document.getElementById("meat").value>0)
-        foodDelievered += "<b>Meat:</b> "+document.getElementById("meat").value+"kg<br>";
-    if (document.getElementById("soup").value>0)
-        foodDelievered += "<b>Soup:</b> "+document.getElementById("soup").value+"kg<br>";
-    document.getElementById("maintext").innerHTML = "<h1>Thank You "+foodStore.selected+" for donating: <br></h1>"+foodDelievered;
+    //Check if nothing is donated
+    if (document.getElementById("rice").value<=0 & document.getElementById("veg").value<=0 & document.getElementById("meat").value<=0 & document.getElementById("soup").value<=0){
+        document.getElementById("maintext").innerHTML = "<h1>YOU DIDN'T EVEN DONATE ANYTHING!</h1>";
+        //Set Return function
+        document.getElementById("maintext").innerHTML += "<input type='submit' value = 'Go Back' onclick='setFood()' '>";
+    }
 
+    //Print what is donated
+    else {
+        if (document.getElementById("rice").value>0)
+            foodDelievered += "<b>Rice:</b> "+document.getElementById("rice").value+"kg<br>";
+        if (document.getElementById("veg").value>0)
+            foodDelievered += "<b>Vegetables:</b> "+document.getElementById("veg").value+"kg<br>";
+        if (document.getElementById("meat").value>0)
+            foodDelievered += "<b>Meat:</b> "+document.getElementById("meat").value+"kg<br>";
+        if (document.getElementById("soup").value>0)
+            foodDelievered += "<b>Soup:</b> "+document.getElementById("soup").value+"kg<br>";
+        document.getElementById("maintext").innerHTML = "<h1>Thank You "+foodStore.selected+" for donating: <br></h1>"+foodDelievered;
+    }
 }
