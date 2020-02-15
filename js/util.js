53040cb503bee95a886312e20f53bbf5bc592193
FremontTeng <fremontteng@gmail.com>
2020-02-15 14:07:24 +0800

    Fix dateTime Minutes

--- js/util.js
+++ js/util.js
@@ -1,5 +1,9 @@
 //Get Date-Time
 getDateTime = function(){
     var d = new Date();
-    document.getElementById("dateTime").innerHTML = d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes();
+    var min = d.getMinutes();
+    //Check if Minutes is in 1 digit
+    if (min<10)minStr = "0"+min;
+    else minStr = min;
+    document.getElementById("dateTime").innerHTML = d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear()+" "+d.getHours()+":"+minStr;
 }
