f1c407bd6d9bdde63ed58ce02379ec4b6581a3d8
FremontTeng <fremontteng@gmail.com>
2020-02-16 01:38:42 +0800

    Add seconds to clk

--- js/util.js
+++ js/util.js
@@ -3,8 +3,10 @@ getDateTime = function(){
     var d = new Date();
     var min = d.getMinutes();
     //Check if Minutes is in 1 digit
-    if (min<10)minStr = "0"+min;
-    else minStr = min;
+    if (min<10)minStr = "0"+min+":";
+    else minStr = min+":";
+    if (d.getSeconds()<10)minStr +="0"+d.getSeconds();
+    else minStr +=d.getSeconds();
     document.getElementById("dateTime").innerHTML = d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear()+" "+d.getHours()+":"+minStr;
 
     //Set TimeOut and loop
