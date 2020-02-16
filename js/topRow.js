4c284516a683f3f302f254550260994096dbd72a
Your Nameansonfu886 <you@example.comansonfu886@gmail.com>
2020-02-16 12:10:13 +0800

    css

--- js/topRow.js
+++ js/topRow.js
@@ -4,18 +4,12 @@ var organizationNames = [
     {"name":"TOUCH Community Services", "address":"162 Bukit Merah Central, #05-3545, Singapore 150162", "link":"https://www.touch.org.sg/"},
     {"name":"Adventist Community Services", "address":"130 Joo Seng Rd, #03-01, Singapore 368357", "link":"https://www.communityservices.org.sg/"},
 ];
-var foodStallNames = [
-    {"name":"ChickenRice", "address":"North Spine"},
-    {"name":"MixedVegetableRice", "address":"North Spine"},
-    {"name":"Vegetarian", "address":"North Spine"},
-    {"name":"DuckRice", "address":"South Spine"},
-    {"name":"NasiBriyani", "address":"South Spine"}
-];
 
 //Contact US Page
 showContactUsPage = function(){
     hideAllPages();
     document.getElementById("maintext").innerHTML = "<h1>Contact Us</h1>"+
+    "<img src='img/MIA.png' alt='MIA logo'><br><br>"+
     "<h2>We welcome any comments, suggestions and feedbacks.</h2>"+
     "Please do drop us an email at shareforfood@mia.com.sg or call us at +65 8765 4321 for any enquiries.";
 }
@@ -27,26 +21,28 @@ showOrganizationListPage = function(){
     for (var i=0;i<organizationNames.length;i++){
     document.getElementById("maintext").innerHTML += "<div class='store2'><div class='storeImg'>" +
         "<a target='_blank' rel='noopener noreferrer' href = '"+organizationNames[i]["link"]+"'><img src='img/" + organizationNames[i]["name"] + ".png' alt='" + organizationNames[i]["name"] +
-         "' width = '250' height ='200'></div><b>" + organizationNames[i]["name"] +"</b><br>Address: "+organizationNames[i]["address"]+"</div></a>";
+         "' width = '250' height ='200'></div>" + organizationNames[i]["name"] +"<br>Address: "+organizationNames[i]["address"]+"</div></a>";
     }
 }
 
 //List of Food Stalls Page
 showFoodStallsListPage = function(){
     hideAllPages();
-    for (var i=0;i<foodStallNames.length;i++){
-        document.getElementById("maintext").innerHTML += "<div class='store'><div class='storeImg'>" +
-            "<a target='_blank' rel='noopener noreferrer'><img src='img/" + foodStallNames[i]["name"] + ".jpg' alt='" + foodStallNames[i]["name"] +
-             "' width = '250' height ='200'></div><b>" + foodStallNames[i]["name"] +"</b><br>Address: "+foodStallNames[i]["address"]+"</div></a>";
-        }
+    //document.getElementById("homePage").style.display = "inline-block";
+    document.getElementById("maintext").innerHTML = 
+    "<div class = imgWrapper>"+
+    "<img src='img/charmander.jpg' alt='Share for Food Poster'>"+
+    "</div>";
 }
 
 //About Us Page
 showAboutUsPage = function(){
     hideAllPages();
+    //document.getElementById("aboutUsPage").style.display = "inline-block";
     document.getElementById("maintext").innerHTML = 
     "<div class= 'centralizeText' id='aboutUsText'>"+
     "<h1>Team MIA</h1>"+
+    "<img src='img/MIA.png' alt='MIA logo'><br><br>"+
     "Members of Team MIA: Fre<b>M</b>ont, N<b>I</b>cholas, <b>A</b>nson<br><br>"+
     "<b>MISSION</b><br>"+
     "Climate change is a change in the common weather found in a location, which can be measured as a difference in how much rain a place usually gets in a year or a change in a location's normal temperature for a month or season. "+
