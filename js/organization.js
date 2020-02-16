43ac3697e2614f4478fd10f2bfd2b6f5faa135ef
FremontTeng <fremontteng@gmail.com>
2020-02-16 14:59:55 +0800

    Revert

--- js/organization.js
+++ js/organization.js
@@ -206,15 +206,15 @@ organizationVerificationPage = function () {
     try { meat = document.getElementById("meatCheck").checked; } catch (error) { }
     try { soup = document.getElementById("soupCheck").checked; } catch (error) { }
     //Check list of organizations
-    txt = "";
-    for (var i =0;i<organizationNames.length;i++){
+    //txt = "";
+    /*for (var i =0;i<organizationNames.length;i++){
         txt += "<option value='"+organizationNames[i]["name"]+"'>"+organizationNames[i]["name"]+"</option>"
     }
-    
+    */
     //Display organization input box
     document.getElementById("subtext1").innerHTML = "<br>Choose your store: <select id='selectedStore'>"+
     txt+"</select><br><br>";
-    //document.getElementById("subtext1").innerHTML = "<b>Organization:</b> <input type='text' id='organizationName' value='Anson and Friends'></input><br>";
+    document.getElementById("subtext1").innerHTML = "<b>Organization:</b> <input type='text' id='organizationName' value='Anson and Friends'></input><br>";
     //Display verification code
     document.getElementById("subtext1").innerHTML += "<b>Verification Passcode:</b> <input type='text' id='verifyCode' value='******'></input><br>";
     //Display food selected
